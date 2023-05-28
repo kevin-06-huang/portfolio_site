99b0060049c5fe2d4201d69c34319f4fbf60ddb1
Kevin <kevin.06.huang@gmail.com>
2023-05-28 14:27:17 -0700

    Start playing with Grommet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,23 +10,17 @@ const Home = () => {
         </div>
         <div className="bragging-point">
           <div className="img query"></div>
-          <h2 className="description">
-            I like to code!
-          </h2>
+          <h2 className="description">I like to code!</h2>
         </div>
       </div>
       <div className="column right">
         <div className="bragging-point top">
           <div className="img lightning"></div>
-          <h2 className="description">
-            This site uses CircleCI & Netlify!
-          </h2>
+          <h2 className="description">This site uses CircleCI & Netlify!</h2>
         </div>
         <div className="bragging-point bottom-right">
           <div className="img normalization"></div>
-          <h2 className="description">
-            I'm still working on Testing!
-          </h2>
+          <h2 className="description">I'm still working on Testing!</h2>
         </div>
       </div>
     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,24 @@
+import { Box, Grid } from "grommet";
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <Grid
+      areas={[
+        { name: "nav", start: [0, 0], end: [0, 0] },
+        { name: "main", start: [1, 0], end: [1, 0] },
+        { name: "side", start: [2, 0], end: [2, 0] },
+        { name: "technologies", start: [0, 1], end: [2, 1] },
+      ]}
+      columns={["flex", "flex", "flex"]}
+      rows={["medium", "small"]}
+      gap="small"
+    >
+      <Box gridArea="nav" background="brand" />
+      <Box gridArea="main" background="brand" />
+      <Box gridArea="side" background="brand" />
+      <Box gridArea="technologies" background="accent-1" />
+    </Grid>
+  );
 };
 
 export default Portfolio;
