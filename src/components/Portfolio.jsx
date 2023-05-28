11a3fdbbd1d1de55a8c317e5ebc75d7c1f047018
Kevin <kevin.06.huang@gmail.com>
2023-05-28 14:50:36 -0700

    Added a card; must style more; added icons

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,35 @@
-import { Box, Grid } from "grommet";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+} from "grommet";
+import { Favorite, ShareOption } from "grommet-icons";
 
 const Portfolio = () => {
   return (
     <Grid
       areas={[
-        { name: "nav", start: [0, 0], end: [0, 0] },
         { name: "main", start: [1, 0], end: [1, 0] },
-        { name: "side", start: [2, 0], end: [2, 0] },
         { name: "technologies", start: [0, 1], end: [2, 1] },
       ]}
-      columns={["flex", "flex", "flex"]}
+      columns={["xxsmall", "flex", "xxsmall"]}
       rows={["medium", "small"]}
-      gap="small"
     >
-      <Box gridArea="nav" background="brand" />
-      <Box gridArea="main" background="brand" />
-      <Box gridArea="side" background="brand" />
-      <Box gridArea="technologies" background="accent-1" />
+      <Box margin="small" gridArea="main" background="dark-3">
+        <Card height="small" width="small" background="light-1">
+          <CardHeader pad="medium">Header</CardHeader>
+          <CardBody pad="medium">Body</CardBody>
+          <CardFooter pad={{ horizontal: "small" }} background="light-2">
+            <Button icon={<Favorite color="red" />} hoverIndicator />
+            <Button icon={<ShareOption color="plain" />} hoverIndicator />
+          </CardFooter>
+        </Card>
+      </Box>
+      <Box margin="small" gridArea="technologies" background="accent-1" />
     </Grid>
   );
 };
