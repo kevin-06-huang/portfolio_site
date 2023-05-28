fd002fae42073a262371e75266fd4afdf7f96ae2
Kevin <kevin.06.huang@gmail.com>
2023-05-27 17:53:58 -0700

    Added config.yml, change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return (<div>Template React App</div>)
-}
+  return (
+    <div>
+      Template React App
+      <p>
+        Successfully Deployed <code>A React application</code> to Netlify with
+        CircleCI
+      </p>
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
